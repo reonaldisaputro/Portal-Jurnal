7fe0640471d3eb46791205de2cf41706cb49d451
reonaldi1105 <reonaldi1105@gmail.com>
2024-09-30 12:30:20 +0000

    fixing ui author

--- resources/views/front/author.blade.php
+++ resources/views/front/author.blade.php
@@ -153,10 +153,10 @@
 			
 		</div>
 	</section>
-	<section id="Advertisement" class="max-w-[1130px] mx-auto flex justify-center mt-[70px]">
-		<div class="flex flex-col gap-3 shrink-0 w-fit">
+	<section id="Advertisement" class="max-w-[1130px] md:mx-auto mx-3 flex justify-center mt-[70px]">
+		<div class="flex flex-col gap-3 shrink-0 w-full">
 			<a href="{{$bannerads->link}}">
-				<div class="w-[900px] h-[120px] flex shrink-0 border border-[#EEF0F7] rounded-2xl overflow-hidden">
+				<div class="w-full h-[120px] flex shrink-0 border border-[#EEF0F7] rounded-2xl overflow-hidden">
 					<img src="{{Storage::url($bannerads->thumbnail)}}" class="object-cover w-full h-full" alt="ads" />
 				</div>
 			</a>
