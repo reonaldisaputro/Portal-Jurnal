90dc4c66e34a2486a4eed54e3bbabef43be61b0e
nurhadighi24 <nurhadi.ghifari24@gmail.com>
2025-01-17 22:35:39 +0700

    style: slicing landing page

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,13 +1,11 @@
-<footer class="bg-[#1A143C] text-white mt-10 pt-20">
-    <div class="grid md:grid-cols-3 gap-5 mb-5 px-20">
+<footer class="bg-white pt-20 pb-20">
+    <div class="flex gap-5 px-20">
         <div>
-            <h2 class="font-bold text-xl">Tentang PKU-MI</h2>
-            <p class="my-3">Pendidikan Kader Ulama Masjid Istiqlal (PKU-MI) adalah platform akademik yang menghimpun
-                jurnal dan karya ilmiah dari mahasiswa di seluruh Indonesia. Kami berkomitmen untuk menjadi pusat
-                rujukan ilmu pengetahuan dan kajian Islam, sekaligus memajukan dakwah digital di era modern.</p>
-            <p class="font-bold text-xl">Ikuti Kami</p>
-            <p>Terhubung dengan kami di media sosial untuk mendapatkan update terbaru:</p>
-            <ul class="list-disc text-white ml-5 pl-5">
+            <a href="{{ route('front.index') }}" class="flex shrink-0">
+                <img src="{{ asset('assets/images/logos/pku-logo.png') }}" alt="logo" />
+            </a>
+            <p class="my-3">Clarity gives you the blocks and components you need to create a truly professional website.</p>
+            <ul class="font-thin  pl-2 flex gap-5 text-black">
                 <li>Facebook</li>
                 <li>Instagram</li>
                 <li>Linkedin</li>
@@ -15,22 +13,25 @@
             </ul>
         </div>
         <div>
-            <h2 class="font-bold text-xl">Navigasi</h2>
-            <ul class="list-disc text-white ml-5 pl-5">
+            <h2 class="font-bold text-xl text-[#2563EB]">Navigasi</h2>
+            <ul class=" font-thin ">
                 <li><a href="{{ route('front.index') }}" class="hover:underline">Beranda</a></li>
                 <li>Jurnal Kategori</li>
                 <li>Penulis & universitas</li>
             </ul>
-            <h3 class="font-bold mt-5 text-xl">Jurnal Terbaru</h3>
-            <ul class="list-disc text-white ml-5 pl-5">
+        </div>
+        <div>
+            <h3 class="font-bold text-xl text-[#2563EB]">Jurnal Terbaru</h3>
+            <ul class=" font-thin ">
                 <li>Pentingnya Literasi Digital bagi ulama Muda di Era Modern</li>
                 <li>Dampak Ekonomi Syariah terhadap Pertumbuhan UMKM di Indonesia</li>
                 <li>Perkembangan Bank Syariah di Indonesia Pasca Pandemi</li>
             </ul>
+
         </div>
         <div>
-            <h2 class="font-bold text-xl">Kontak Kami</h2>
-            <ul class="list-disc ml-5 pl-5">
+            <h2 class="font-bold text-xl text-[#2563EB]">Kontak Kami</h2>
+            <ul class=" ">
                 <li><span class="font-bold">Alamat: </span>Masjid Istiqlal, Jl.Taman Wijaya Kusuma, Jakarta Pusat,
                     Indonesia</li>
                 <li><span class="font-bold">Email: </span>info@pkumi.ac.id</li>
@@ -38,7 +39,7 @@
             </ul>
         </div>
     </div>
-    <hr class="md:mx-16 mx-10">
+    {{-- <hr class="md:mx-16 mx-10">
     <div class="md:flex justify-between items-center py-5 px-20 md:mx-20">
         <div class="logo-container flex gap-[30px] items-center">
             <a href="{{ route('front.index') }}" class="flex shrink-0">
@@ -48,5 +49,5 @@
         <div>
             <p>© 2024 Pendidikan Kader Ulama Masjid Istiqlal. Semua hak cipta dilindungi.</p>
         </div>
-    </div>
+    </div> --}}
 </footer>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav id="Navbar" class="w-max my-10 mx-auto px-5 py-2 flex justify-between items-center bg-white rounded-lg h-max">
+<nav id="Navbar" class="w-max my-10 mx-auto px-5 py-2 flex justify-between items-center rounded-lg h-max">
     <div class="logo-container flex gap-[30px] items-center">
         <a href="{{ route('front.index') }}" class="flex shrink-0">
             <img src="{{ asset('assets/images/logos/pku-logo.png') }}" alt="logo" />
@@ -14,22 +14,34 @@
 
     <!-- Navigation Links (hidden on mobile, shown on larger screens) -->
     <div id="menu" class="hidden md:flex md:gap-[30px] items-center">
-        <a href="{{ route('front.index') }}" class="text-[#1A143C] font-bold hover:underline">Beranda</a>
+        <a href="{{ route('front.index') }}" class="text-white font-bold hover:underline">Beranda</a>
 
-        <p class="text-[#1A143C] font-bold">Jurnal Kategori</p>
+        <p class="text-white font-bold">Berita</p>
 
-        <p class="text-[#1A143C] font-bold">Penulis</p>
-        {{-- <p class="text-[#1A143C] font-bold">Universitas</p> --}}
-        <form method="GET" action="{{ route('front.search') }}"
+        <p class="text-white font-bold">Tentang PKU</p>
+        <p class="text-white font-bold">Akademik</p>
+        <p class="text-white font-bold">Penelitian</p>
+        <p class="text-white font-bold">Galeri</p>
+        <p class="text-white font-bold">Kontak Kami</p>
+        <p class="text-white font-bold">Siakad PKU</p>
+        <div class="flex gap-2">
+            <img src="" alt="avatar">
+            <p class="text-white font-bold">Behzad</p>
+        </div>
+        <svg width="49" height="73" viewBox="0 0 49 73" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M30.0249 27C31.0405 27 31.8999 27.8594 31.8999 28.875V45.75C31.8999 46.7266 30.8452 47.3125 29.9858 46.8438L24.3999 43.5625L18.7749 46.8438C17.9155 47.3125 16.8999 46.7266 16.8999 45.75V28.875C16.8999 27.8594 17.7202 27 18.7749 27H30.0249ZM30.0249 44.6562V29.1094C30.0249 28.9922 29.9077 28.875 29.7515 28.875H18.9702C18.853 28.875 18.7749 28.9922 18.7749 29.1094V44.6562L24.3999 41.375L30.0249 44.6562Z" fill="white"/>
+        </svg>
+            
+        {{-- <form method="GET" action="{{ route('front.search') }}"
             class="w-[450px] flex items-center rounded-full border border-[#E8EBF4] p-[12px_20px] gap-[10px] focus-within:ring-2 focus-within:ring-[#FF6B18] transition-all duration-300">
-            @csrf
+            @csrf --}}
             <button type="submit" class="w-5 h-5 flex shrink-0">
                 <img src="{{ asset('assets/images/icons/search-normal.svg') }}" alt="icon" />
             </button>
-            <input type="text" name="keyword" id=""
+            {{-- <input type="text" name="keyword" id=""
                 class="appearance-none outline-none w-full font-semibold placeholder:font-normal placeholder:text-[#A3A6AE]"
                 placeholder="Cari artikel atau berita..." />
-        </form>
+        </form> --}}
     </div>
 </nav>
 
